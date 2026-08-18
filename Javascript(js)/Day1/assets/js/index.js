27ec25c2cd1cde55133592ed9349447c99015f9a
S. SRI AKASH <sriakashsrinivasan19@gmail.com>
2026-08-18 23:51:58 +0530

    update all

---
+++ Javascript(js)/Day1/assets/js/index.js
@@ -0,0 +1,301 @@
+// Day-1 Task-1
+
+// using var
+// var passWord;
+// var emailAddress;
+// var userName;
+// var date_time;
+// var logIn;
+// var subjectName;
+// var info_about;
+// var areaName;
+// var number_phone;
+// var _javaScript;
+// var _addtion;
+// var _id;
+// var dateOfBirth;
+// var staffId;
+// var _subtraction;
+// var userRole;
+// var userAge;
+// var itemCount;
+// var domainName;
+// var outputValue;
+// var scopeType;
+// var classRoom;
+// var setValue;
+// var floorNumber;
+// var bottleWeight;
+// var wireType;
+// var laptopNumber;
+// var chairBrand;
+// var slaVision;
+// var _displayValue;
+// var _websiteName;
+// var _html;
+// var _ground;
+// var _company;
+// var _school;
+// var _brand;
+// var _clothesType;
+// var _outlook;
+// var_multiple;
+// var _doctype;
+// var _mapLocation;
+// var mathValue;
+// var styleGuide;
+// var objectModel;
+// var totalPrice;
+// var productItem;
+// var currentIndex;
+// var currentTime;
+
+// // using let
+// let _count;
+// let _sum;
+// let _score;
+// let userName;
+// let priceAmount;
+// let _quantity;
+// let totalValue;
+// let _grade;
+// let _message;
+// let userAge;
+// let productName;
+// let productPrice;
+// let itenCount;
+// let loginStatus;
+// let _password;
+// let searchText;
+// let selectedItem;
+// let searchBar;
+// let currentPage;
+// let selectedSize;
+// let pageNumber;
+// let _product;
+// let errorMessage;
+// let cartCount;
+// let currentDate;
+// let currentMonth;
+// let currentYear;
+// let _availabe;
+// let _active;
+// let _inActive;
+// let userPhone;
+// let _tax;
+// let bonusAmount;
+// let _discount;
+// let salaryAmount;
+// let _points;
+// let rankValue;
+// let _percentage;
+// let _level;
+// let resultTrue;
+// let resultFalse;
+// let loggedIn;
+// let sizeValue;
+// let colorValue;
+// let bagroundValue;
+// let cursorPoint;
+// let orderStatus;
+// let _available;
+
+// // using const
+// const _pi;
+// const maxValue;
+// const minValue;
+// const companyName;
+// const schoolName;
+// const doctorId;
+// const imageUrl;
+// const databaseName;
+// const tableName;
+// const webpageCount;
+// const stateName;
+// const  _language;
+// const _color;
+// const _baground;
+// const _theme;
+// const taxRate;
+// const discountRate;
+// const salaryValue;
+// const max_Length;
+// const min_Length;
+// const max_size;
+// const min_sixe;
+// const page_size;
+// const start_Data;
+// const end_data;
+// const current_weak;
+// const current_Year;
+// const server_Url;
+// const login_Url;
+// const logout_Url;
+// const support_Email;
+// const company_Gst;
+// const school_Count;
+// const sla_Institute;
+// const _time;
+// const_day;
+// const _month;
+// const _year;
+// const_ relative name;
+// const data_Format;
+// const currency_Number;
+// const keyWords;
+// const _usernumber;
+// const _userEmail;
+// const _userAge;
+// const _dataType;
+// const loopHole;
+
+
+// Day-2 Task-1
+
+// using var="string";
+
+var userName="sriAkash";
+console.log(userName);
+var userAddress="chennai";
+console.log(userAddress);
+var userCountry="india";
+console.log(userCountry);
+var userEmail="sriakash19@gmail.com";
+console.log(userEmail);
+var userNumber="9840986931";
+console.log(userNumber);
+var userAge="21";
+console.log(userAge);
+var aboutUser="he Is A Jovial Person";
+console.log(aboutUser);
+var hospitalName="akHospital";
+console.log(hospitalName);
+var instituteName="slaInstitute";
+console.log(instituteName);
+var projectName="techSpere";
+console.log(projectName);
+var domainName="fullstack Development";
+console.log(domainName);
+var date_Time="18-08-2026_13:40";
+console.log(date_Time);
+var logIn="entry Your Id";
+console.log(logIn);
+var userRole="frontend Developer";
+console.log(userRole);
+var appName="vsCode";
+console.log(appName);
+var websiteName="goole Chrome";
+console.log(websiteName);
+var _html="semantic Tags";
+console.log(_html);
+var clothesType="cotton Material";
+console.log(clothesType);
+
+// using let=number;
+
+let _number=1234;
+console.log(_number);
+let _add= 1+2;
+console.log(_add);
+let _sub=2-3;
+console.log(_sub);
+let _multiple=2;
+console.log(_multiple);
+let _divide=4/2;
+console.log(_divide);
+let _percentage=30+"%";
+console.log(_percentage);
+let _value=20;
+console.log(_value);
+let _score=20/30;
+console.log(_score);
+let totalValue=100;
+console.log(totalValue);
+let totalMarks=500;
+console.log(totalMarks);
+let gradeValue=50;
+console.log(gradeValue);
+let _userAge=18;
+console.log(_userAge);
+let shirtPrice=749;
+console.log(shirtPrice);
+let productNumber=12;
+console.log(productNumber);
+let currentDate=18082026;
+console.log(currentDate);
+let currentTime=1353;
+console.log(currentTime);
+let currentMonth=8;
+console.log(currentMonth);
+let currentYear=2026;
+console.log(currentYear);
+
+// using const;
+
+const isName= "sri";
+console.log(isName);
+const isPassword= 1234;
+console.log(isPassword);
+const isAge= 18;
+console.log(isAge);
+const isEmail= "gmail.com";
+console.log(isEmail);
+const price= 20000;
+console.log(price);
+
+// USING NULL
+
+let taxValue = null;
+console.log(taxValue);
+let subValue = null;
+console.log(subValue);
+let initialValue = null;
+
+// using undefined
+let projectValue;
+console.log(projectValue);
+
+let studyTime;
+console.log(studyTime);
+
+let studyHour;
+console.log(studyHour);
+
+
+
+// using console.log();
+console.log(userName);
+console.log(userAddress);
+console.log(userCountry);
+console.log(userEmail);
+console.log(userNumber);
+console.log(userAge);
+console.log(aboutUser);
+console.log(hospitalName);
+console.log(instituteName);
+console.log(domainName);
+console.log(_number);
+console.log(_add);
+console.log(_sub);
+console.log(_multiple);
+console.log(_divide);
+console.log(_percentage);
+console.log(_value);
+console.log(_score);
+console.log(totalValue);
+console.log(totalMarks);
+console.log(price);
+console.log(isName);
+console.log(isPassword);
+console.log(isEmail);
+console.log(isAge);
+console.log(taxValue);
+console.log(subValue);
+console.log(initialValue);
+console.log(projectValue);
+console.log(studyTime);
+console.log(studyHour);
+
+
+
+
