ea6bbfaaf73e069ce78e7c070abb32a966d8e688
S. SRI AKASH <sriakashsrinivasan19@gmail.com>
2026-08-20 14:33:49 +0530

    upadate all

---
+++ Javascript(js)/Day3/assets/js/index.js
@@ -0,0 +1,69 @@
+// basic calculation
+let shirtPrice = 650;
+let pantPrice = 800;
+
+console.log(shirtPrice + pantPrice);
+console.log(shirtPrice - pantPrice);
+console.log(shirtPrice * pantPrice);
+console.log(shirtPrice / pantPrice);
+console.log(shirtPrice % pantPrice);
+
+
+// assignment operators
+let collegeFees = 20000;
+collegeFees += 100;
+console.log(collegeFees);
+
+let shoePrice = 650;
+shoePrice -= 100;
+console.log(shoePrice);
+
+let amountValue = 30;
+amountValue *= 2;
+console.log(amountValue);
+
+let medicinePrice = 50;
+medicinePrice /= 2;
+console.log(medicinePrice);
+
+// comparison operator
+
+let voterAge = 18;
+console.log(voterAge == 19);
+console.log(voterAge === 18);
+console.log(voterAge > 16);
+console.log(voterAge < 19);
+console.log(voterAge >= 20);
+console.log(voterAge <= 20);
+
+// logical operator
+
+let priceValue = 200;
+let shopId = true;
+
+console.log(priceValue >= 300 && shopId);
+console.log(priceValue <= 300 || shopId);
+console.log( !shopId);
+
+// increament and decreament
+
+let shoePairs = 2;
+shoePairs ++;
+console.log(shoePairs);
+shoePairs ++;
+console.log(shoePairs);
+shoePairs --;
+console.log(shoePairs);
+shoePairs --;
+console.log(shoePairs);
+
+
+
+
+
+
+
+
+
+ 
+ 
