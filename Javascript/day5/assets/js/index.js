9e7e4d80d6a4ffa2fc393363451d724176b01e72
S. SRI AKASH <sriakashsrinivasan19@gmail.com>
2026-08-23 22:53:56 +0530

    update day5

---
+++ Javascript/day5/assets/js/index.js
@@ -0,0 +1,50 @@
+for (let numberPlus = 1; numberPlus <= 10; numberPlus++) {
+    console.log(numberPlus);
+}
+
+for (let numberMinus = 10; numberMinus >= 1; numberMinus--) {
+    console.log(numberMinus);
+}
+
+for (let number_Plus = 1; number_Plus <= 20; number_Plus++) {
+    console.log(number_Plus);
+}
+
+for (let evenNumber = 2; evenNumber <= 20; evenNumber += 2) {
+    console.log(evenNumber);
+}
+
+for (let oddNumber = 1; oddNumber <= 20; oddNumber += 2) {
+    console.log(oddNumber);
+}
+
+for (let orderValue = 1; orderValue <= 10; orderValue++) {
+    console.log(orderValue);
+}
+
+for (let evenOrder = 2; evenOrder <= 20; evenOrder += 2) {
+    console.log(evenOrder);
+}
+
+for (let oddOrder = 1; oddOrder <= 20; oddOrder += 2) {
+    console.log(oddOrder);
+}
+
+let number = 5;
+
+for (let multipleNumber = 1; multipleNumber <= 10; multipleNumber++) {
+    console.log(number + " x " + multipleNumber + " = " + number * multipleNumber);
+}
+
+
+let sum = 0;
+
+for (let addNumber = 1; addNumber <= 10; addNumber++) {
+    sum = sum + addNumber;
+}
+
+console.log(sum);
+
+for (let descValue = 10; descValue >= 5; descValue--) {
+    console.log(descValue);
+}
